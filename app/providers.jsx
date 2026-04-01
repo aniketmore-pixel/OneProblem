df2b7655594031a8f4a70bb16937a5ac373f828f
aniketmore-pixel <aniketmore.personal@gmail.com>
2026-04-01 14:45:18 +0530

    Initial Commit

--- app/providers.jsx
+++ app/providers.jsx
@@ -2,15 +2,13 @@
 
 import { SessionProvider } from 'next-auth/react'
 import { Toaster } from 'react-hot-toast'
-import StoreProvider from '@/app/StoreProvider'
 
 export default function Providers({ children }) {
   return (
     <SessionProvider>
-      <StoreProvider>
+    
         <Toaster />
         {children}
-      </StoreProvider>
     </SessionProvider>
   )
 }
